dbe6dd08a4025a717ba9eea931919d77a6b546ea
sumania95 <ayansumania95@gmail.com>
2021-06-23 18:04:07 +0800

    edit printing JVF logo

--- application/page_summary_report/views.py
+++ application/page_summary_report/views.py
@@ -19,6 +19,7 @@ from django.template.loader import render_to_string
 from django.template import RequestContext
 from django.contrib.auth.mixins import LoginRequiredMixin
 from application.models import (
+    Receiving,
     Releasing,
 )
 
@@ -40,3 +41,55 @@ class Summary_Report_Page(LoginRequiredMixin,TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = "Summary Report"
         return context
+
+class Summary_Receiving_Table_AJAXView(LoginRequiredMixin,View):
+    queryset = Receiving.objects.all()
+    template_name = 'tables/receiving_table.html'
+    def get(self, request):
+        data = dict()
+        try:
+            search = self.request.GET.get('search')
+            start = self.request.GET.get('start')
+            end = self.request.GET.get('end')
+            datepicker1 = self.request.GET.get('datepicker1')
+            datepicker2 = self.request.GET.get('datepicker2')
+        except KeyError:
+            search = None
+            start = None
+            end = None
+            datepicker1 = None
+            datepicker2 = None
+        date_from = datetime.strptime(datepicker1+' 00:00:00', "%Y-%m-%d %H:%M:%S")
+        date_to = datetime.strptime(datepicker2+' 23:59:59', "%Y-%m-%d %H:%M:%S")
+        if search or start or end or date_from or date_to:
+            data['form_is_valid'] = True
+            data['counter'] = self.queryset.filter(Q(vendor__icontains = search),branch=self.request.user.user_type.branch,date_created__range = [date_from,date_to]).count()
+            receiving = self.queryset.filter(Q(vendor__icontains = search),branch=self.request.user.user_type.branch,date_created__range = [date_from,date_to]).annotate(number_of_receiving_detail=Sum('receiving_detail__quantity')).order_by('-date_created')[int(start):int(end)]
+            data['data'] = render_to_string(self.template_name,{'receiving':receiving,'start':start})
+        return JsonResponse(data)
+
+class Summary_Releasing_Table_AJAXView(LoginRequiredMixin,View):
+    queryset = Receiving.objects.all()
+    template_name = 'tables/receiving_table.html'
+    def get(self, request):
+        data = dict()
+        try:
+            search = self.request.GET.get('search')
+            start = self.request.GET.get('start')
+            end = self.request.GET.get('end')
+            datepicker1 = self.request.GET.get('datepicker1')
+            datepicker2 = self.request.GET.get('datepicker2')
+        except KeyError:
+            search = None
+            start = None
+            end = None
+            datepicker1 = None
+            datepicker2 = None
+        date_from = datetime.strptime(datepicker1+' 00:00:00', "%Y-%m-%d %H:%M:%S")
+        date_to = datetime.strptime(datepicker2+' 23:59:59', "%Y-%m-%d %H:%M:%S")
+        if search or start or end or date_from or date_to:
+            data['form_is_valid'] = True
+            data['counter'] = self.queryset.filter(Q(vendor__icontains = search),branch=self.request.user.user_type.branch,date_created__range = [date_from,date_to]).count()
+            receiving = self.queryset.filter(Q(vendor__icontains = search),branch=self.request.user.user_type.branch,date_created__range = [date_from,date_to]).annotate(number_of_receiving_detail=Sum('receiving_detail__quantity')).order_by('-date_created')[int(start):int(end)]
+            data['data'] = render_to_string(self.template_name,{'receiving':receiving,'start':start})
+        return JsonResponse(data)
--- application/views.py
+++ application/views.py
@@ -29,8 +29,15 @@ class Home(LoginRequiredMixin,TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        product = Product.objects.filter(branch=self.request.user.user_type.branch).aggregate(quantity=Coalesce(Sum('quantity'),0))['quantity']
+        receiving = Receiving.objects.filter(branch=self.request.user.user_type.branch).aggregate(quantity=Coalesce(Sum('receiving_detail__quantity'),0))['quantity']
+        releasing = Releasing.objects.filter(branch=self.request.user.user_type.branch).aggregate(quantity=Coalesce(Sum('releasing_detail__quantity'),0))['quantity']
+        context['check_balance'] = int(product)+int(releasing)-int(receiving)
+        context['product_quantity_total'] = Product.objects.filter(branch=self.request.user.user_type.branch).aggregate(quantity=Coalesce(Sum('quantity'),0))
         context['product_total'] = Product.objects.filter(branch=self.request.user.user_type.branch).count()
         context['customer_total'] = Customer.objects.filter(branch=self.request.user.user_type.branch).count()
+        context['receiving_quantity_total'] = Receiving.objects.filter(branch=self.request.user.user_type.branch).aggregate(quantity=Coalesce(Sum('receiving_detail__quantity'),0))
         context['receiving_total'] = Receiving.objects.filter(branch=self.request.user.user_type.branch).count()
+        context['releasing_quantity_total'] = Releasing.objects.filter(branch=self.request.user.user_type.branch).aggregate(quantity=Coalesce(Sum('releasing_detail__quantity'),0))
         context['releasing_total'] = Releasing.objects.filter(branch=self.request.user.user_type.branch).count()
         return context
